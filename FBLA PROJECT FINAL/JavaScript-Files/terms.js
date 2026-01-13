cc957a11ab0c6dc8909934f690dfc18c34dfcfc2
Amer Mohamed Salih <ame2164160@maricopa.edu>
2026-01-13 13:34:32 -0700

    Delete FBLA PROJECT FINAL directory

--- FBLA PROJECT FINAL/JavaScript-Files/terms.js
+++
@@ -1,9 +0,0 @@
-//Terms.js
-
-//Burger menu
-const burger = document.getElementById("burger");
-const menu = document.querySelector(".menu");
-
-burger.addEventListener("click", () => {
-    menu.classList.toggle("show");
-});
